af9a5f64048ed1f3076624096d88102f5b75cefd
Diasah1 <diana.samueeel05@gmail.com>
2025-11-27 13:10:01 +0100

    tagit bort onödig kod

--- PodCastApplikation/Models/Klasser/Podd.cs
+++ PodCastApplikation/Models/Klasser/Podd.cs
@@ -9,20 +9,20 @@ namespace PodCastApplikation.Models.Klasser
 {
     public class Podd
     {
-        [BsonId] // anger att detta fält är dokumentets unika ID i MongoDB
+        [BsonId] 
         [BsonRepresentation(BsonType.ObjectId)]
 
-        public string? Id { get; set; } // get/set tillåter läsning/skrivning
-        public string? OriginalTitel { get; set; } // Titel från RSS-flödet
+        public string? Id { get; set; } 
+        public string? OriginalTitel { get; set; }
 
-        public string? AnvändarTitel { get; set; } //Titel som användaren döpt podden till
-        public string? Beskrivning { get; set; } //Poddens beskrvning från RSS
+        public string? AnvändarTitel { get; set; } 
+        public string? Beskrivning { get; set; } 
 
-        public string? RssURL { get; set; } //RSS-adressen anv'ndaren skriver in
+        public string? RssURL { get; set; } 
 
-        public string? KategoriId { get; set; } // Vilken kategori podden tillhör
+        public string? KategoriId { get; set; } 
 
-        public List<Avsnitt> Avsnitt { get; set; } = new(); //varje gång en Podd skapas, så skapas automatiskt en tom lista. 
+        public List<Avsnitt> Avsnitt { get; set; } = new(); 
 
     }
 }
